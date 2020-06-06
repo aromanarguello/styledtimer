bd3d78742df161cc5e9720d924e510ed50964157
Alejandro <aromanarguello@gmail.com>
2020-06-06 09:51:47 -0400

    workshop commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-function App() {
+const Container = styled.div`
+  border: ${({ theme }) => `3px solid ${theme.darkTheme.darkGray[1]}`};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.darkGray};
+`;
+
+const Button = styled.button`
+  background-color: ${({ success, danger }) => (success ? "green" : danger ? "red" : null)};
+`;
+
+const List = styled.li`
+  color: ${(props) => (props.time % 2 === 0 ? "red" : "blue")};
+`;
+
+const App = () => {
+  const [isActive, toggleActive] = useState(false);
+  const [initialTime, setInitialTime] = useState(0);
+  const [times, setTimes] = useState([]);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setInitialTime((prev) => prev + 1);
+      }, 1000);
+    } else if (!isActive && initialTime !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, initialTime]);
+
+  const handleReset = () => {
+    toggleActive(false);
+    setInitialTime(0);
+  };
+
+  const handleRecord = () => {
+    setTimes((prev) => [...prev, initialTime]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Timer: {initialTime}</Title>
+      <Button success onClick={() => toggleActive((prev) => !prev)}>
+        {isActive ? "Pause" : "Start"}
+      </Button>
+      <Button danger onClick={handleReset}>
+        Reset
+      </Button>
+      {!isActive && initialTime !== 0 ? (
+        <Button onClick={handleRecord} success>
+          Record
+        </Button>
+      ) : null}
+      {times.length > 0 ? (
+        <ul>
+          {times.map((time) => (
+            <List time={time}>{time}</List>
+          ))}
+        </ul>
+      ) : null}
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/Theme.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  darkTheme: {
+    darkGray: ["pink", "purple"],
+    darkerGray: "#32363d",
+  },
+  peachTheme: {
+    darkGray: "#353a3f",
+    darkerGray: "#32363d",
+  },
+  colors: [],
+};
+
+const Theme = (props) => {
+  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+};
+
+export default Theme;
